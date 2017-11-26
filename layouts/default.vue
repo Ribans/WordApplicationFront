cd223faf2c7fb3ebd6958631f20dee32b7e12922
JpnLavender <irimamekun@gmail.com>
2017-11-26 21:50:10 +0900

    Logoutの実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,9 +7,9 @@
         <b-navbar-brand href="/">Ankimo</b-navbar-brand>
         <b-collapse is-nav id="nav_collapse">
           <b-nav is-nav-bar>
-            <b-nav-item href="/english_apps/learn">記憶</b-nav-item>
-            <b-nav-item href="/english_apps/challenge">挑戦</b-nav-item>
-            <b-nav-item href="/english_apps/training">練習</b-nav-item>
+            <b-nav-item href="/english_apps/learn">記憶モード</b-nav-item>
+            <b-nav-item href="/english_apps/challenge">実力テスト</b-nav-item>
+            <b-nav-item href="/english_apps/training">トレーニング</b-nav-item>
           </b-nav>
 
           <b-nav is-nav-bar class="ml-auto">
@@ -17,7 +17,7 @@
               <template slot="button-content"> <strong>User</strong> </template>
               <div v-if="$store.state.currentUser && !($store.state.currentUser.statusCode == 403) ">
                 <b-dropdown-item href="/users/profile">Profile</b-dropdown-item>
-                <b-dropdown-item href="/logout">Logout</b-dropdown-item>
+                <b-dropdown-item v-on:click="logout">Logout</b-dropdown-item>
               </div>
               <div v-else>
                 <b-dropdown-item href="/users/signin">Signin</b-dropdown-item>
@@ -43,6 +43,11 @@ export default {
   data: function() {
     return {
       userLogined: false,
+    }
+  },
+  methods: {
+    async logout() {
+      await this.$store.dispatch('logout')
     }
   }
 }
